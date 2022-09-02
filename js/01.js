1c6aaa7be106c697f5d06893b73357ada565ae59
JPintor81 <106607339+JPintor81@users.noreply.github.com>
2022-09-02 17:06:56 -0500

    Reglas de Variables y Const
    Algunas reglas de las variables en Java y las principales características de la constante ¨Const¨

--- js/01.js
+++ js/01.js
@@ -1,6 +1,14 @@
-// variables 
+// Reglas de la variabeles
+/* 
+    * La variables no pueden iniciar con nnumeros o caracteres especiales
+    * (Guion bajo si esta permitido)
+    * La variables no pueden tener espacios
+    * Se utiliza como Regala general se utiliza camelCase
+*/
 
-/*
+// variables elt
+
+/*    
     * En Java hay 3 formas de crear variables
         1. var / esta es la forma antigua pero ya casi no se usa
         2. let
---
+++ js/02.js
@@ -0,0 +1,13 @@
+// Variables "const"
+
+/*
+    * Las variables const no se pueden re-asignar
+    * No se puede inciar una "const" sin valores
+    * const es mas utilizado que el let
+*/
+
+const cliente = "Jorge"
+const precioDescuento = 220
+
+console.log(cliente)
+console.log(precioDescuento)
