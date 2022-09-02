2aca1973e16b87652511c9c18e20a23cabf7cb47
JPintor81 <106607339+JPintor81@users.noreply.github.com>
2022-09-02 16:58:10 -0500

    Variables let
    Explicación y Ejemplos de variables let

---
+++ js/01.js
@@ -0,0 +1,17 @@
+// variables 
+
+/*
+    * En Java hay 3 formas de crear variables
+        1. var / esta es la forma antigua pero ya casi no se usa
+        2. let
+        3. const
+    * Se pueden Iniciar sin ningun valor
+    * Se pueden Reasignar
+    * En Java Se untiliza camelCase
+*/
+
+let cliente = "Jorge"
+let precioDescuento = 200
+
+console.log(precioDescuento) 
+console.log(cliente) 
