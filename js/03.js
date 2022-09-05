880091a8029fc0fadfebb409ed4b0db58668f7a2
JPintor81 <106607339+JPintor81@users.noreply.github.com>
2022-09-05 10:29:58 -0500

    Tipo de Datos
    Se relacionan los principales tipos de datos que se utilizan en JavaScrip

---
+++ js/03.js
@@ -0,0 +1,61 @@
+// Tipos de Datos
+
+// *Undefined: Se traduce literalmente a indefinida, por lo cual cuando dejamos una variable sin valor esta automaticamente se define asi misma como indefinida
+
+
+/* let cliente = 20
+
+console.log(typeof cliente)
+console.log(cliente) 
+
+cliente = undefined
+
+console.log(typeof cliente)
+console.log(cliente)  */
+
+
+//* Boolean = true o False;
+
+/* const descuento = true;
+
+console.log(typeof descuento)
+console.log(descuento) */
+
+//* Number: A diferencia de otros lenguajes de programacion, Number trata a todos los numeros por igual todo los numeros son de tipo "NUMBER".
+
+/* const number1 = 20.20
+const number2 = 30
+const number3 = -100
+
+console.log(typeof number1)
+console.log(typeof number2)
+console.log(typeof number3) */
+
+//* String: Son cadenas de Texto
+
+/* const nombre = "Jorge"
+const nombre2 = 'Jorge'
+const numero = 30 // Esta variable es de tipo Number
+const numero2 = "30" //Al tener comillas dobles o sencillas lo convierten en String */
+
+//* BigInt: Esta se usa para almacenar numeros muy grandes, pero para definir esta variable hay que definirla con "BigInt(*)", estos datos no se pueden mezclar con otros datos de tipo number.
+
+/* const numberBigInt = BigInt(71908419238410293841029384213749128340298484409287542)
+
+console.log(numberBigInt)
+console.log(typeof numberBigInt)
+ */
+
+//* Symbol
+/*   const primerSymbolo = Symbol(30)
+  const segundoSymbolo = Symbol(30)
+
+  console.log(primerSymbolo === segundoSymbolo)//Imprime False */
+
+  // Todos los simbolos son diferentes por eso siempre imprime "False"
+
+//* Null se condidera como valores Primitivos
+
+/* const ejemplo = null
+console.log(ejemplo) */
+
