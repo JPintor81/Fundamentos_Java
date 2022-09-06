934f2a81d092a84f86cd1f763601ce7ddd943ae2
JPintor81 <106607339+JPintor81@users.noreply.github.com>
2022-09-05 19:13:15 -0500

    Funciones Y Métodos Útiles Para Arreglos
    17. Funciones - Function declaration
    
    18. Funciones - Parámetros y Parámetros por Default
    
    19. Funciones que retornan valores
    
    20. Funciones - Function Expression
    
    21. Funciones - Arrow Functions
    
    22. Arrow Functions y Array Methods
    
    23. Otros Array Methods útiles

---
+++ js/15.js
@@ -0,0 +1,21 @@
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js']
+
+/* const nuevoArray = tecnologias.map( function(tech) {
+    if(tech === 'HTML') {
+        return 'GraphQL'
+    } else {
+        return tech
+    } */
+
+// const nuevoArray = tecnologias.map( tech => {
+//     if(tech === 'HTML') {
+//         return 'GraphQL'
+//     } else {
+//         return tech
+//     }
+// })
+
+const nuevoArray2 = tecnologias.filter( tech => tech !== 'React')
+
+// console.log(nuevoArray)
+console.log(nuevoArray2)
